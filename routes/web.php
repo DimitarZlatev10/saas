0c12e44fdf23b2a3590797347a513ca43da96a8a
Dimitar Zlatev <dimitarzlatev02@gmail.com>
2024-08-08 16:32:18 +0300

    updated routes

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,35 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use App\Livewire\BusinessDetails;
+use App\Livewire\BusinessList;
+use App\Livewire\BusinessRegister;
+use App\Livewire\EditBusiness;
+use App\Livewire\Home;
+use App\Livewire\UserAppointments;
+use App\Livewire\UserBusinesses;
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'welcome');
+// Authentication routes
+Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])->name('logout');
 
+// User Routes
+Route::middleware(['auth'])->group(function () {
+    Route::get('/my-businesses', UserBusinesses::class)->name('user.businesses');
+    Route::get('/my-appointments', UserAppointments::class)->name('user.appointments');
+    Route::get('/businesses/{businessId}/edit', EditBusiness::class)->name('business.edit');
+});
+
+// Home Route
+Route::get('/', Home::class)->name('home');
+
+// Business routes
+Route::get('/businesses', BusinessList::class)->name('businesses.index');
+Route::get('/businesses/register', BusinessRegister::class)->name('businesses.register');
+Route::get('/businesses/{business}', BusinessDetails::class)->name('businesses.show');
+
+
+// Additional routes
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
     ->name('dashboard');
@@ -12,4 +38,5 @@ Route::view('profile', 'profile')
     ->middleware(['auth'])
     ->name('profile');
 
-require __DIR__.'/auth.php';
+// Authentication routes (Breeze or default)
+require __DIR__ . '/auth.php';
